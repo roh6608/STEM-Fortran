5935d0989c9ef047d3fcc0fa5f87e765be682fc0
roh6608 <rohan.sthill@gmail.com>
2021-06-08 20:49:56 +1000

    the start of the plot module

--- src/plot.f90
+++ src/plot.f90
@@ -1,8 +1,20 @@
 module plot
     implicit none
-
     contains
-        
+
+        subroutine plot2d(x, y)
+            real, intent(in), dimension(:) :: x, y
+            integer :: i
+            
+            open(unit = 1, file = 'data.dat')
+
+            do i = 1, size(x)
+                write(1,*) x(i), ' ', y(i)
+            end do
+        end subroutine plot2d
+
+
+
 
 
 end module plot
